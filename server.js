21989c6559f51260b5d49ca40dd7333aa50e58ba
kwlucas <lucaswkyle@gmail.com>
2022-10-22 11:24:32 -0600

    Created and set up sever.js

---
+++ server.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./routes');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const helpers = require('./utils/helpers');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Session/Cookie maxAge
+//60000 is one minute
+//300000 is five minutes
+//900000 is fifteen minutes
+
+const sess = {
+  secret: 'Test Secret',
+  cookie: {
+    maxAge: 900000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
