fae0686173168fba0c1c2a168370648a6e4915e8
kwlucas <lucaswkyle@gmail.com>
2022-10-25 12:07:13 -0600

    Added in log-out route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -57,3 +57,15 @@ router.post('/login', async (req, res) => {
         res.status(500).send({ message: 'The server encountered an error!' });
     }
 });
+
+router.post('/logout', (req, res) => {
+    console.log('log out');
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+
+        });
+    } else {
+        res.status(404).end();
+    }
+});
