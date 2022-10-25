82f5b18254197ac72b1bb105bbdf54d56b097bcd
kwlucas <lucaswkyle@gmail.com>
2022-10-25 11:58:29 -0600

    Added route to create new user

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
+
+            res.status(201).json(userData);
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
