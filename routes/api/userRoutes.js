604519747e111b36605c488cf1db5df0dee83ff6
kwlucas <lucaswkyle@gmail.com>
2022-10-25 12:05:40 -0600

    Added sign-in route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -17,3 +17,43 @@ router.post('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+        //Find user associated with entered username
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+            attributes: {
+                //Fields which won't be included in response data
+                exclude: ['password']
+            }
+        });
+
+        if (!user) {
+            res.status(400).json({ message: 'Invalid user credentials!' });
+            return;
+        }
+
+        //Check if password is valid
+        const validPassword = user.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Invalid user credentials!' });
+            return;
+        }
+        //console.log('accepted user and password');
+        //Session save
+        req.session.save(() => {
+            req.session.user_id = user.id;
+            req.session.username = user.username;
+            req.session.loggedIn = true;
+
+            res.json({ user, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: 'The server encountered an error!' });
+    }
+});
