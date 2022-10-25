bdda22e6dad2b037c30805246879c02898efa387
kwlucas <lucaswkyle@gmail.com>
2022-10-25 12:43:14 -0600

    Added update post route

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -20,3 +20,29 @@ router.post('/', async (req, res) => {
         res.status(500).send({ message: 'The server encountered an error!' });
     }
 });
+
+router.put('/:id', async (req, res) => {
+    try {
+        if (req.session.userId) {
+            const postData = await Post.update(req.body, {
+                where: {
+                    id: req.params.id,
+                    user_id: req.session.userId
+                }
+            });
+
+            if (postData) {
+                res.status(200).json(postData);
+            }
+            else {
+                res.status(404).send({ message: 'Post not found!' });
+            }
+        }
+        else {
+            res.status(401).send({ message: 'Unauthorized request!' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: 'The server encountered an error!' });
+    }
+});
