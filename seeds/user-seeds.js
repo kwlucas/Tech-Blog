08df04553dc2f81a18801f84d732e03fe661fbef
kwlucas <lucaswkyle@gmail.com>
2022-11-16 12:02:04 -0700

    Specified user seed ids

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,18 +2,22 @@ const { User } = require('../models');
 
 const userData = [
     {
+        id: 1,
         username: 'IamTheFirst',
         password: 'Password1'
     },
     {
+        id: 2,
         username: 'WebDevGirl',
         password: 'Password2'
     },
     {
+        id: 3,
         username: 'Base2Binary',
         password: 'Password3'
     },
     {
+        id: 4,
         username: 'TheAncientGeek',
         password: 'Password4'
     },
