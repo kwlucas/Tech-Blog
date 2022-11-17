9774e552533c7faf95c61659966a88459f545a81
kwlucas <lucaswkyle@gmail.com>
2022-11-17 11:30:19 -0700

    Added in get signed in user function

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,4 +1,19 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
+async function getSignedInUser(r) {
+    let signedInUser = ''
+    //Check if user is signed in
+    if (r.session.user_id) {
+        //Get user that is signed in
+        signedInUser = await User.findByPk(r.session.user_id, {
+            attributes: {
+                exclude: ['password']
+            }
+        });
+        signedInUser = signedInUser.get({ plain: true });
+    }
+    return signedInUser;
+}
+
 module.exports = router;
