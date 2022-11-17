79379665e5e2b22d9543cd3031775a3c853c63a1
kwlucas <lucaswkyle@gmail.com>
2022-11-17 13:30:50 -0700

    Created event handlers for editing posts

--- public/scripts/adjustContent.js
+++ public/scripts/adjustContent.js
@@ -16,4 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         document.location.replace(`/post/${newPost.id}`);
     });
+
+    document.querySelector('#edit-post-form').addEventListener('reset', function () {
+        document.location.replace('/dashboard');
+    });
+
+    document.querySelector('#edit-post-form').addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const post_id = document.querySelector('[post_id]').getAttribute('post_id');
+        const title = document.querySelector('#title-input').textContent;
+        const content = document.querySelector('#content-input').textContent;
+        await fetch(`/api/posts/${post_id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title,
+                content
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        document.location.replace('/dashboard');
+    });
 })
