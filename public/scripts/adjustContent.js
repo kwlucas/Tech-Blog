005cff06b84e9f748aac2d24677a33ed6ead81d6
kwlucas <lucaswkyle@gmail.com>
2022-11-17 13:32:09 -0700

    Created event handler for deleting comments

--- public/scripts/adjustContent.js
+++ public/scripts/adjustContent.js
@@ -57,4 +57,15 @@ document.addEventListener('DOMContentLoaded', () => {
         //Reload the page (while preserving scroll position) 
         document.location.reload();
     });
+
+    (document.querySelectorAll('[id^="delete-btn-"]') || []).forEach(deleteBtnEl => {//get all elements with and id that starts with "delete-btn-" and run for each
+        //Get the "comment_id" of the post from the element id
+        const commentId = deleteBtnEl.id.match(/(?<=delete-btn-)\d+$/)[0];
+        deleteBtnEl.addEventListener('click', async function () {
+            const response = await fetch(`/api/comments/${commentId}`, {
+                method: 'DELETE'
+            });
+            document.location.replace('/dashboard');
+        });
+    });
 })
