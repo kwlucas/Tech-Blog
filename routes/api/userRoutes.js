c15de602c23ae61a1f17be22841b566aac0f335a
kwlucas <lucaswkyle@gmail.com>
2022-11-19 13:31:06 -0700

    Removed password exclusion form login fetch

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -20,21 +20,18 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
+        console.log(req.body.username);
         //Find user associated with entered username
         const user = await User.findOne({
             where: {
                 username: req.body.username,
-            },
-            attributes: {
-                //Fields which won't be included in response data
-                exclude: ['password']
             }
         });
         if (!user) {
             res.status(400).json({ message: 'Invalid user credentials!' }).end();
         }
         console.log(user);
-        console.log(req.body);
+        console.log(req.body.password);
         //Check if password is valid
         const validPassword = user.checkPassword(req.body.password);
 
