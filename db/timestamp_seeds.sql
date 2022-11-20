59a2bae6617a51c530620d80575b2dcaed0c9b8d
kwlucas <lucaswkyle@gmail.com>
2022-11-20 12:06:11 -0700

    Updated time stamps for comments on post 2

--- db/timestamp_seeds.sql
+++ db/timestamp_seeds.sql
@@ -3,4 +3,10 @@ UPDATE Comment SET created_at = '2022-11-16 19:29:10' WHERE id = 1;
 UPDATE Comment SET created_at = '2022-11-16 19:29:11' WHERE id = 2;
 
 UPDATE Post SET created_at = '2022-11-16 20:30:00' WHERE id = 2;
+UPDATE Comment SET created_at = '2022-11-16 19:31:00' WHERE id = 3;
+UPDATE Comment SET created_at = '2022-11-16 20:30:00' WHERE id = 4;
+UPDATE Comment SET created_at = '2022-11-17 10:00:00' WHERE id = 5;
+
 UPDATE Post SET created_at = '2022-11-17 15:30:00' WHERE id = 3;
+UPDATE Comment SET created_at = '' WHERE id = 6;
+UPDATE Comment SET created_at = '' WHERE id = 7;
