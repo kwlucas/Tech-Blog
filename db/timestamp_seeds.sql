465b84be875e9f06e2c6c730927c67b9d9ba1cfe
kwlucas <lucaswkyle@gmail.com>
2022-11-20 11:56:03 -0700

    Added updated timestamps for post seeds

---
+++ db/timestamp_seeds.sql
@@ -0,0 +1,3 @@
+UPDATE Post SET created_at = '2022-11-16 19:29:09' WHERE id = 1;
+UPDATE Post SET created_at = '2022-11-16 20:30:00' WHERE id = 2;
+UPDATE Post SET created_at = '2022-11-17 15:30:00' WHERE id = 3;
