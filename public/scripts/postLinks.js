57801f3d8b801858f81b06c2ab50f443a90c1dd1
kwlucas <lucaswkyle@gmail.com>
2022-11-23 12:21:17 -0700

    Add comment to post links scripts

--- public/scripts/postLinks.js
+++ public/scripts/postLinks.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     (document.querySelectorAll('.card[post_id]') || []).forEach(postEl => {//get all elements with "log-btn" class and run for each
         //Get the "post_id" of the post from the element attribute
         const postId = postEl.getAttribute('post_id');
+        //Add an event listener to post that when clicked it redirects to that post's page
         postEl.addEventListener('click', function () {
             document.location.replace(`/post/${postId}`);
         });
